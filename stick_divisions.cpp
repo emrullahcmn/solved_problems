119751b19750367c3e5680422e7014854bd3ed96
emo_test <emo_test>
2023-01-28 21:51:44 +0300

    a greedy solution

---
+++ stick_divisions.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h> 
+#include <set> 
+#include <map> 
+#include <unordered_map> 
+#include <vector> 
+#include <utility> //pair
+#include <string>
+#define F first 
+#define S second 
+#define PB push_back 
+#define MP make_pair 
+typedef long long ll; 
+using namespace std; 
+
+int main() { 
+
+  int x,n;
+  cin >> x >> n;
+
+  multiset<ll> ms;
+  ll tmp;
+  for(int i = 0; i<n; i++){
+    cin >> tmp;
+    ms.insert(tmp); 
+  }
+
+  ll ans = 0;
+  while(ms.size() > 1){
+    auto a = *ms.begin();
+    auto b = *(++ms.begin());
+    ans += a+b;
+    ms.erase(ms.find(a));
+    ms.erase(ms.find(b));
+    ms.insert(a+b);
+  }
+
+  cout << ans << endl;
+
+return 0;
+}
